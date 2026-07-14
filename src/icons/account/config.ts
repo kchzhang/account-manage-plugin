0099dad6faad4de4c1b6733672a413951bd0e764
Knox <kchzhang@icloud.com>
2026-07-14 18:59:42 +0800

    feat(account): 新增 IconFont 图标

--- src/icons/account/config.ts
+++ src/icons/account/config.ts
@@ -2,6 +2,7 @@ import jenkinsIconUrl from './jenkins.png?url';
 import jumpserverIconUrl from './jumpserver.png?url';
 import gitlabIconUrl from './gitlab.png?url';
 import defaultIconUrl from './default.svg?url';
+import iconfontIconUrl from './iconfont.svg?url';
 
 export interface AccountIconOption {
   key: string;
@@ -14,6 +15,7 @@ export const ACCOUNT_ICON_OPTIONS: AccountIconOption[] = [
   { key: 'jumpserver', label: 'JumpServer', url: jumpserverIconUrl },
   { key: 'gitlab', label: 'GitLab', url: gitlabIconUrl },
   { key: 'default', label: 'Default', url: defaultIconUrl },
+  { key: 'iconfont', label: 'IconFont', url: iconfontIconUrl },
 ];
 
 export function getAccountIconUrl(iconKey: string): string {
